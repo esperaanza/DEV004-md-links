2a172d0c9df812f142cc3a9edd2b8089b8447318
Esperaanza <xcaanda100@gmail.com>
2023-04-29 22:06:54 -0600

    primer codigo

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-console */
+import mdLinks from './api';
 
-export const mdLinks = () => {
-  // ...
-};
+// Ejemplo de uso con validate:false
+mdLinks('./README.md', { validate: false })
+  // eslint-disable-next-line no-console
+  .then((links) => console.log(links))
+  .catch((err) => console.error(err));
 
-
-
+// Ejemplo de uso con validate:true
+mdLinks('./README.md', { validate: true })
+  .then((links) => console.log(links))
+  .catch((err) => console.error(err));
