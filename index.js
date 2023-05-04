5f287e574ce5174628cf9c289f49e2b4ff0aaa78
Esperaanza <xcaanda100@gmail.com>
2023-05-04 09:31:47 -0600

    prieras funciones

--- index.js
+++ index.js
@@ -1,13 +1,105 @@
-/* eslint-disable no-console */
-import mdLinks from './api';
 
-// Ejemplo de uso con validate:false
-mdLinks('./README.md', { validate: false })
-  // eslint-disable-next-line no-console
-  .then((links) => console.log(links))
-  .catch((err) => console.error(err));
+import { existsSync, readFile, readdirSync,statSync} from 'fs';
+import { resolve, extname, isAbsolute } from 'path';
+import chalk from 'chalk';
 
-// Ejemplo de uso con validate:true
-mdLinks('./README.md', { validate: true })
-  .then((links) => console.log(links))
-  .catch((err) => console.error(err));
+
+// valida si existe la ruta
+const validatePath = (path) => existsSync(path);
+
+ // ver si la ruta es absoluta, si es relativa se convierte
+const resolveRelativePath = (path) => {
+   if(isAbsolute(path)) /* <is absolute path?> */ {
+    return path;
+   } else {
+    return resolve(path);
+   }
+}
+//valida si es directorio
+const validateDirectory = (absolutePath) => statSync(absolutePath).isDirectory();
+
+//valida si es un archivo md
+const validateMDFile = (filePath) => extname(filePath) === ".md"; 
+
+//lee el archivo y busca los links
+const readFileAndSearchLinks = (filePath) => {
+  readFile(filePath, 'utf-8', (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    } else {
+      console.log({filePath, result})
+    }
+  })
+}
+//lista archivos del directorio
+const listFilesFromDirectory = (absolutePath) => {
+     const files = readdirSync(absolutePath) 
+     files.forEach(file => {
+       const path = `${absolutePath}/${file}` // concatenamos el path del directorio con el nombre del archivo/directorio
+      if(validateDirectory(path)){
+        listFilesFromDirectory(path); // recursividad
+      } else if (validateMDFile(path)) { // si es md se lee el archivo
+        readFileAndSearchLinks(path)
+      } 
+  })
+}
+
+export const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => { 
+    if (!validatePath(path)) /* <Path exists> */ {
+      reject(new Error('dont exist path'));
+      return;
+    } 
+    console.log('path exists');
+    const absolutePath = resolveRelativePath(path); /* <is abosolute path?> */
+    console.log('path absolute', absolutePath);
+
+      // probar si es un directorio o un archivo
+    if(validateDirectory(absolutePath)) /* <is directory?> */ {
+      console.log(chalk.bgHex('#69FF63').bold('is directory    '));
+       //leemos directorio
+       listFilesFromDirectory(absolutePath);
+       resolve('ok directory')
+    } else if(validateMDFile(absolutePath)) /*<is md?>*/ {
+      console.log('is md file');
+      resolve('ok file')
+    } else {
+      reject('invalid path');
+    }
+  })
+}
+const path = '/Users/Esperanza/proyecto/DEV004-md-links';
+
+
+
+mdLinks(path) // consumiendo la promesa
+.then(result => console.log(result))
+.catch(error => console.log(error))
+
+
+
+// import { resolve as _resolve, isAbsolute as _isAbsolute } from 'path';
+// import { existsSync } from 'fs';
+
+// export function mdLinks(route) {
+//   return new Promise((resolve, reject) => {
+//     let absolutePath = _resolve(route);
+//     let isAbsolute = _isAbsolute(absolutePath);
+//     let isExisting = existsSync(absolutePath);
+//     let result;
+
+//     if (!isExisting) {
+//       result = 'La ruta no existe';
+//     } else if (isAbsolute) {
+//       result = 'Ruta absoluta';
+//     } else {
+//       result = 'Ruta relativa';
+//     }
+//     console.log (resolve(result));
+//     console.log('hola')
+//   });
+// // }
+// mdLinks('C:\Users\Esperanza\proyecto\DEV004-md-links\README.md')
+//   .then(result => console.log(result))
+//   .catch(error => console.error(error));
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,20 @@
 import { mdLinks }from '../index';
 
 
-describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('mdLinks', () => {
+  it('debe resolver la ruta absoluta para una ruta válida', async () => {
+    const ruta = 'C:\\Users\\Esperanza\\Documents\\National Instruments';
+    const result = await mdLinks(ruta);
+    expect(result).toBe('Ruta absoluta');
   });
 
+  it('debe rechazar la promesa para una ruta no existente', async () => {
+    const ruta = 'C:\\Ruta\\No\\Existente';
+    try {
+      return await mdLinks(ruta);
+    } catch (error) {
+      expect(error).toBe('La ruta no existe');
+    }
+  });
 });
